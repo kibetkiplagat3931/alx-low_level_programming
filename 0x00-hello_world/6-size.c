db6aafc02db1f7bef7fb239a66d4197dba3f67df
kibetkiplagat3931 <williamslagat@gmail.com>
2023-03-18 13:58:16 -0700

    size of function c hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - A program that prints the size of variable types using sizeof
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	char c;
+	int i;
+	long int a;
+	long long int d;
+	float f;
+
+	printf("size of a char : %lu byte(s) \n", (unsigned long), size of(c));
+	printf("size of an int :%lu byte(s) \n", (unsigned long), size of(i));
+	printf("size of a long int :%lu byte(s) \n", (unsigned long), size of(a));
+	printf("size of a long long int :%lu byte(s) \n", (unsigned long), size of(d));
+	printf("size of a float :%lu byte(s) \n", (unsigned long), size of(f));
+	return (0);
+}
